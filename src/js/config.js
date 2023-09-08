a03ab6ebd1116b1ca72ca9844ce3f12a5e17cce9
Shahzaib-Shakoor <shahzaib.shakoor2199@gmail.com>
2023-09-08 18:24:01 +0500

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '8b148bf3-2c98-45a6-80b6-d50de165305c';
+export const MODAL_CLOSE_SEC = 2.5;
